a71af3540394e1ed26dc247e33ca99e9998195f2
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-09-23 16:40:49 -0300

    chore[aula 06]: aprendendo forma mais simplificada para trabalhar com evento de click

--- app/src/main/java/com/example/aula01/MainActivity.java
+++ app/src/main/java/com/example/aula01/MainActivity.java
@@ -10,59 +10,60 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     double num1, num2, result;
+
+    Button btn_somar;
+    Button btn_subtrair;
+    Button btn_multiplicar;
+    Button btn_dividir;
+    EditText et_valor1;
+    EditText et_valor2;
+    TextView tv_resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_somar = (Button) findViewById(R.id.btn_somar);
-        Button btn_subtrair = (Button) findViewById(R.id.btn_subtrair);
-        Button btn_multiplicar = (Button) findViewById(R.id.btn_multiplicar);
-        Button btn_dividir = (Button) findViewById(R.id.btn_dividir);
+        btn_somar = findViewById(R.id.btn_somar);
+        btn_subtrair = findViewById(R.id.btn_subtrair);
+        btn_multiplicar = findViewById(R.id.btn_multiplicar);
+        btn_dividir = findViewById(R.id.btn_dividir);
 
-        EditText et_valor1 = (EditText) findViewById(R.id.et_valor1);
-        EditText et_valor2 = (EditText) findViewById(R.id.et_valor2);
+        et_valor1 = findViewById(R.id.et_valor1);
+        et_valor2 = findViewById(R.id.et_valor2);
 
-        TextView tv_resultado = (TextView) findViewById(R.id.tv_resultado);
+        tv_resultado = findViewById(R.id.tv_resultado);
 
-        btn_somar.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                result = num1 + num2;
-                tv_resultado.setText(String.valueOf(result));
-            }
-        });
-
-        btn_subtrair.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                result = num1 - num2;
-                tv_resultado.setText(String.valueOf(result));
-            }
-        });
-
-
-        btn_multiplicar.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                result = num1 * num2;
-                tv_resultado.setText(String.valueOf(result));
-            }
-        });
-
-
-        btn_dividir.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                num1 = Double.parseDouble(et_valor1.getText().toString());
-                num2 = Double.parseDouble(et_valor2.getText().toString());
-                result = num1 / num2;
-                tv_resultado.setText(String.valueOf(result));
-            }
-        });
     }
+
+    public void somar(View v){
+        num1 = Double.parseDouble(et_valor1.getText().toString());
+        num2 = Double.parseDouble(et_valor2.getText().toString());
+        result = num1 + num2;
+        tv_resultado.setText(String.valueOf(result));
+    }
+
+    public void subtrair(View v){
+        num1 = Double.parseDouble(et_valor1.getText().toString());
+        num2 = Double.parseDouble(et_valor2.getText().toString());
+        result = num1 - num2;
+        tv_resultado.setText(String.valueOf(result));
+    }
+
+    public void multiplicar(View v){
+        num1 = Double.parseDouble(et_valor1.getText().toString());
+        num2 = Double.parseDouble(et_valor2.getText().toString());
+        result = num1 * num2;
+        tv_resultado.setText(String.valueOf(result));
+    }
+
+    public void dividir(View v){
+        num1 = Double.parseDouble(et_valor1.getText().toString());
+        num2 = Double.parseDouble(et_valor2.getText().toString());
+        result = num1 / num2;
+        tv_resultado.setText(String.valueOf(result));
+    }
+
+
 
 
 }
