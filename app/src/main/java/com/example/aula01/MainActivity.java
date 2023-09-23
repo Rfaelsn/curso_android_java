5ec4d2e60abd2c5b72b34b7e05ef920ba2ab5660
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-09-22 23:04:04 -0300

    feat[aula 05] desenvolvendo calculadora simples

--- app/src/main/java/com/example/aula01/MainActivity.java
+++ app/src/main/java/com/example/aula01/MainActivity.java
@@ -9,20 +9,57 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    double num1, num2, result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn_somar = (Button) findViewById(R.id.btn_somar);
+        Button btn_subtrair = (Button) findViewById(R.id.btn_subtrair);
+        Button btn_multiplicar = (Button) findViewById(R.id.btn_multiplicar);
+        Button btn_dividir = (Button) findViewById(R.id.btn_dividir);
 
-        Button btn_action = (Button) findViewById(R.id.btn_ativation);
-        TextView tv_result_nome = (TextView) findViewById(R.id.tv_result_name);
-        EditText et_nome = (EditText) findViewById(R.id.et_name);
+        EditText et_valor1 = (EditText) findViewById(R.id.et_valor1);
+        EditText et_valor2 = (EditText) findViewById(R.id.et_valor2);
 
-        btn_action.setOnClickListener(new View.OnClickListener(){
+        TextView tv_resultado = (TextView) findViewById(R.id.tv_resultado);
+
+        btn_somar.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                tv_result_nome.setText(et_nome.getText());
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                result = num1 + num2;
+                tv_resultado.setText(String.valueOf(result));
+            }
+        });
+
+        btn_subtrair.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                result = num1 - num2;
+                tv_resultado.setText(String.valueOf(result));
+            }
+        });
+
+
+        btn_multiplicar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                result = num1 * num2;
+                tv_resultado.setText(String.valueOf(result));
+            }
+        });
+
+
+        btn_dividir.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                num1 = Double.parseDouble(et_valor1.getText().toString());
+                num2 = Double.parseDouble(et_valor2.getText().toString());
+                result = num1 / num2;
+                tv_resultado.setText(String.valueOf(result));
             }
         });
     }
